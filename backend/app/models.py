a75c7538bf738fea6410266074bf4e3de9b5c13b
m-pasha <pv.pasha.pv@gmail.com>
2020-04-09 18:05:59 +0200

    add migrations

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model):
     user = models.ForeignKey(User, related_name="user_movie", on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, blank=True)
-    year = models.CharField(max_length=20, blank=True)
-    actors = models.CharField(max_length=255, blank=True)
-    awards = models.CharField(max_length=255, blank=True)
-    country = models.CharField(max_length=255, blank=True)
-    dvd = models.CharField(max_length=20, blank=True)
-    director = models.CharField(max_length=255, blank=True)
-    genre = models.CharField(max_length=255, blank=True)
-    plot = models.CharField(max_length=500, blank=True)
-    released = models.CharField(max_length=20, blank=True)
-    runtime = models.CharField(max_length=20, blank=True)
-    poster = models.CharField(max_length=500, blank=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    year = models.CharField(max_length=20, blank=True, null=True)
+    actors = models.CharField(max_length=255, blank=True, null=True)
+    awards = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    dvd = models.CharField(max_length=20, blank=True, null=True)
+    director = models.CharField(max_length=255, blank=True, null=True)
+    genre = models.CharField(max_length=255, blank=True, null=True)
+    plot = models.CharField(max_length=500, blank=True, null=True)
+    released = models.CharField(max_length=20, blank=True, null=True)
+    runtime = models.CharField(max_length=20, blank=True, null=True)
+    poster = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.title
